6678d965f2898cba863b873da83aa6a19f717afe
Long Lê Doãn <ledoanlong.bnh@gmail.com>
2022-11-30 13:19:29 +0700

    Check Weather

---
+++ weather.js
@@ -0,0 +1,20 @@
+const APP_ID = '8d26ee93684457121dd10d5a83135875';
+
+const searchInput = document.querySelector('#search-input');
+const cityName = document.querySelector('.city-name');
+const weatherState = document.querySelector('.weather-state');
+const weatherIcon = document.querySelector('.weather-icon');
+const temperature = document.querySelector('.temperature');
+
+const sunrise = document.querySelector('.sunrise');
+const sunset = document.querySelector('.sunset');
+const humidity = document.querySelector('.humidity');
+const windSpeed = document.querySelector('.wind-speed');
+
+searchInput.addEventListener('change', (e) => {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${e.target.value}&appid=${APP_ID}&units=metric&lang=vi`)
+        .then(async res => {
+            const data  = await res.json();
+            console.log('[Search Input]', data);
+        });
+})
